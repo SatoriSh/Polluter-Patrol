cee1432d643c8de4617a8d8ca38af8a6a48d1e4e
SatoriSh <satorizoom51@gmail.com>
2025-08-17 00:22:10 +0200

    Add in Level.cs private _maxPhotographedCharactersCount

--- scenes/levels/Level.cs
+++ scenes/levels/Level.cs
@@ -8,15 +8,18 @@ public partial class Level : Node2D
 
     private int _photographedCharactersCount = 0;
 
+    [Export]
+    private int _maxPhotographedCharactersCount;
+
     public override void _Ready()
     {
-        _labelCountToWin.Text = $"Count {_photographedCharactersCount}";
+        _labelCountToWin.Text = $"Count {_photographedCharactersCount} / {_maxPhotographedCharactersCount}";
     }
 
     public void UpdateLabelCountToWinText()
     {
         _photographedCharactersCount++;
-        _labelCountToWin.Text = $"Count {_photographedCharactersCount}";
+        _labelCountToWin.Text = $"Count {_photographedCharactersCount} / {_maxPhotographedCharactersCount}";
     }
 
 }
