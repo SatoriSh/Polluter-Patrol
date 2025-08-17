aba43616ed61a0895dc9a1c2651323ca5f110478
SatoriSh <satorizoom51@gmail.com>
2025-08-17 14:48:17 +0200

    Add PackedScene[] in LevelsMenu.cs

--- scenes/menu/LevelsMenu.cs
+++ scenes/menu/LevelsMenu.cs
@@ -6,41 +6,56 @@ public partial class LevelsMenu : Node2D
     private PackedScene _menuScene;
 
     [Export]
-    private PackedScene _level0Scene;
-    [Export]
-    private PackedScene _level1Scene;
-    [Export]
-    private PackedScene _level2Scene;
-    [Export]
-    private PackedScene _level3Scene;
-    [Export]
-    private PackedScene _level4Scene;
-    [Export]
-    private PackedScene _level5Scene;
-    [Export]
-    private PackedScene _level6Scene;
-    [Export]
-    private PackedScene _level7Scene;
-    [Export]
-    private PackedScene _level8Scene;
-    [Export]
-    private PackedScene _level9Scene;
+    private string[] _pathToScenes = new string[9];
+    private PackedScene[] _scenes = new PackedScene[9];
 
     [Export]
     private AnimationPlayer _anim;
 
     private bool _animPlaying = false;
 
+    private PackedScene _currentChoiceScene;
+
     public override void _Ready()
     {
         _menuScene = GD.Load<PackedScene>("res://scenes/menu/menu.tscn");
+
+        for (int i = 0; i < _scenes.Length; i++)
+        {
+            if (_pathToScenes[i] != null)
+            {
+                _scenes[i] = GD.Load<PackedScene>(_pathToScenes[i]);
+            }
+        }
     }
 
     private void _on_button_to_menu_button_down()
     {
-        if (!_animPlaying) _anim.Play("change_to_menu");
-        _animPlaying = true;
+        if (!_animPlaying)
+        {
+            _animPlaying = true;
+            _anim.Play("ChangeScene");
+            _currentChoiceScene = _menuScene;
+        }
     }
 
-    public void ChangeSceneToMenu() => GetTree().ChangeSceneToPacked(_menuScene); 
+    private void _on_levelbutton_3_button_down()
+    {
+        if (!_animPlaying)
+        {
+            SetAnimAndCurrentScene(3);
+        }
+    }
+
+    private void SetAnimAndCurrentScene(int sceneIndex)
+    {
+        _animPlaying = true;
+        _anim.Play("ChangeScene");
+        _currentChoiceScene = _scenes[sceneIndex];
+    }
+
+    public void ChangeScene()
+    {
+        GetTree().ChangeSceneToPacked(_currentChoiceScene);
+    }
 }
