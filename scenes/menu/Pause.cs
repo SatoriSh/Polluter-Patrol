3ec06913bbabc0c6082d8f47e52a893b6a0f75e5
SatoriSh <satorizoom51@gmail.com>
2025-08-17 19:52:50 +0200

    Remove level 3 and update level/menu scenes

--- scenes/menu/Pause.cs
+++ scenes/menu/Pause.cs
@@ -17,13 +17,21 @@ public partial class Pause : Node2D
 
     private bool _animPlaying = false;
 
+    Timer timer = new Timer();
+    private bool _canOpenPauseMenu = true;
+
     public override void _Ready()
     {
         _anim.AnimationFinished += OnAnimationFinished;
+
+        timer.WaitTime = 0.5f;
+        timer.Timeout += _on_timer_timeout;
+        AddChild(timer);
     }
 
     public override void _Process(double delta)
     {
+        if (!_canOpenPauseMenu) return;
         if (Input.IsActionJustPressed("pause") && _animFinished)
         {
             UpdateWorldPause();
@@ -36,6 +44,9 @@ public partial class Pause : Node2D
         _worldInPause = !_worldInPause;
         GetTree().Paused = _worldInPause;
         _anim.Play(_worldInPause ? "show" : "hide");
+
+        _canOpenPauseMenu = false;
+        timer.Start();
     }
 
     private void _on_button_pause_off_button_down()
@@ -66,4 +77,6 @@ public partial class Pause : Node2D
     public void ChangeSceneToLevels() => GetTree().ChangeSceneToPacked(_levelsScene);
 
     private void OnAnimationFinished(StringName anim) => _animFinished = true;
+
+    private void _on_timer_timeout() => _canOpenPauseMenu = true;
 }
